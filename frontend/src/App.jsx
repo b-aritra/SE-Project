2b534b77c88503ba747ae56eb80f81d3c10ea9a2
Atul Rajput <eatulrajput@gmail.com>
2025-03-18 17:19:00 +0530

    Add Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointments from './pages/Appointments'
+import Navbar from './components/Navbar'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      
-    </>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors:speciality' element={<Doctors />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointments' element={<Appointments />} />
+
+
+      </Routes>
+
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { use, useState } from 'react'
+import { assets } from '../assets/assets'
+import { NavLink, useNavigate } from 'react-router-dom'
+
+
+const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] =  useState(false)
+    const [token, setToken] = useState(true)
+
+    return (
+        <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+
+            <img className='w-44 cursor-pointer' src={assets.logo} alt="Logo" />
+
+            <ul className='hidden md:flex items-start gap-5 font-medium '>
+
+                <NavLink to='/' className={({ isActive }) => isActive ? 'text-indigo-400 font-semibold border-b-2 border-indigo-400 pb-1' : 'text-gray-700'}>
+                    <li className='py-1'>Home</li>
+                    <hr className='border-none outline-none h-0.5 bg-indigo-400 w3/5 m-auto hidden' />      
+                    </NavLink>
+
+                <NavLink to='/doctors' className={({ isActive }) => isActive ? 'text-indigo-400 font-semibold border-b-2 border-indigo-400 pb-1' : 'text-gray-700'}>
+                    <li className='py-1'>All Doctors</li>
+                    <hr className='border-none outline-none h-0.5 bg-indigo-400 w3/5 m-auto hidden' />
+                </NavLink>
+
+                <NavLink to='/about' className={({ isActive }) => isActive ? 'text-indigo-400 font-semibold border-b-2 border-indigo-400 pb-1' : 'text-gray-700'}>
+                    <li className='py-1'>About</li>
+                    <hr className='border-none outline-none h-0.5 bg-indigo-400 w3/5 m-auto hidden' />       
+                    </NavLink>
+
+                <NavLink to='/contact' className={({ isActive }) => isActive ? 'text-indigo-400 font-semibold border-b-2 border-indigo-400 pb-1' : 'text-gray-700'}>
+                    <li className='py-1'>Contact</li>
+                    <hr className='border-none outline-none h-0.5 bg-indigo-400 w3/5 m-auto hidden' />         
+                    </NavLink>
+            </ul>
+            <div className='flex items-center'>
+                
+                {
+                    token 
+                    ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                        <img className='w-8 rounded-full' src={assets.profile_pic} alt="profile picture" />
+                        <img className='w-2.5' src={assets.dropdown_icon} alt="profile picture" />
+                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                        <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                        <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                        <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                        <p onClick={()=> setToken(false)} className='hover:text-black cursor-pointer'>Logout </p>
+                        </div>
+                    </div>
+                    </div>
+                    : <button onClick={()=>navigate('/login')} className='bg-indigo-400 text-white px-8 py-3 rounded-full font-light hidden md:block hover:cursor-pointer'>Create Account</button>
+                }
+                
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
