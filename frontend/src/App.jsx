567f76b6136c06feb23908ba2bfe1d9c25f760a7
Atul Rajput <eatulrajput@gmail.com>
2025-03-19 10:56:58 +0530

    Add Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Login from './pages/Login'
 import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
-import Appointments from './pages/Appointments'
+import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
@@ -24,7 +24,7 @@ const App = () => {
         <Route path='/contact' element={<Contact />} />
         <Route path='/my-profile' element={<MyProfile />} />
         <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/appointments:docId' element={<Appointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
       <Footer />
     </div>
